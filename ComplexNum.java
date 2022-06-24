08b7c5dd10ec356f53999344a6268bc9b4109552
Pritamkhan2025 <107670962+Pritamkhan2025@users.noreply.github.com>
2022-06-24 10:59:27 +0530

    Update ComplexNum.java

--- ComplexNum.java
+++ ComplexNum.java
@@ -10,20 +10,20 @@ class ComplexNum{
 	{  
 		Scanner num = new Scanner(System.in);
 
-		System.out.println("Enter 1st Real number:");
+		System.out.println("Enter 1st Real number:");	//real number input from user end
 		int a1Real = num.nextInt();
 
-		System.out.println("Enter 2nd Real number:");
+		System.out.println("Enter 2nd Real number:");	
 		int a2Real = num.nextInt();
 
-		System.out.println("Enter 1st Imaginary number:");
+		System.out.println("Enter 1st Imaginary number:");	//imaginary number input from user end
 		int b1Img = num.nextInt();
 
 		System.out.println("Enter 2nd Imaginary number:");
 		int b2Img = num.nextInt();
 
-		int sumReal = a1Real + a2Real; //sum of real numbers
-		int sumImg = b1Img + b2Img;    //sum of imaginary numbers
+		int sumReal = a1Real + a2Real; 	//sum of real numbers
+		int sumImg = b1Img + b2Img;   	 //sum of imaginary numbers
 
 		System.out.println("Sum of two complex number is: "+sumReal + "+" + +sumImg + "i");
 		
