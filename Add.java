6c9060614ab673d1612a513b789ca4bf2b3dd6b0
Pritamkhan2025 <107670962+Pritamkhan2025@users.noreply.github.com>
2022-06-24 10:55:53 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -9,7 +9,7 @@ class Add{
 	
 	int a = 20;
 	int b = 30;
-	int sum = a + b;//addition two numbers
+	int sum = a + b; //addition two numbers and store the value
 
 	System.out.println("Addition of two number is "+sum);
 
