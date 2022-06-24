3172ec571e41d42869ae80781c5dc69f0fe38c17
Pritamkhan2025 <107670962+Pritamkhan2025@users.noreply.github.com>
2022-06-24 10:53:52 +0530

    Update GetInput.java

--- GetInput.java
+++ GetInput.java
@@ -7,7 +7,7 @@ class GetInput{
 	public static void main(String[] args)  
 	{  
 		Scanner name= new Scanner(System.in);  
-		System.out.print("Enter a string: ");  
+		System.out.print("Enter a string: ");  //input from user end
 		String str= name.nextLine();             
 		System.out.print("Input form user: "+str);             
 	}  
